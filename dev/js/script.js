3c86a893d025a421ba8729d6115c2cde82e94dcb
Yuriy Buranov <Yura962Bur@mail.ru>
2021-09-03 16:43:28 +0400

    feat: end mobile version for the site

--- dev/js/script.js
+++ dev/js/script.js
@@ -77,29 +77,3 @@ function getTimeRemaining(endtime) {
     'seconds': seconds
   };
 }
- 
-function initializeClock(id, endtime) {
-  var clock = document.querySelector(id);
-  var daysSpan = clock.getElementById('#days');
-  var hoursSpan = clock.getElementById('#hours');
-  var minutesSpan = clock.getElementById('#minutes');
- 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
- 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
- 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
- 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
- 
-var deadline = new Date(Date.parse(new Date()) + 94 * 12 * 20 * 1000); // for endless timer
-initializeClock('timer', deadline);
